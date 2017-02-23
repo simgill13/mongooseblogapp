2dc21a1a2710296f61602b1c8a19e2257e1b8e74
Sim Gill <simgill89@gmail.com>
2017-02-23 14:12:45 -0800

    updates congif file

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://testuser:testpassword@ds161099.mlab.com:61099/mongblog';
+                      'mongodb://localhost/mongblog';
 exports.PORT = process.env.PORT || 8080;
